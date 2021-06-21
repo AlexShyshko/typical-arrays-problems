651d968c4e7f4ed9962e9fffd6df8ed43606c1dd
AlexShyshko <shish1988@mail.ru>
2021-06-21 16:09:39 +0300

    feat: write functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array === undefined || array == 0) {
+      return 0;
+    }
+    let minimal = array[0];
+    let i;
+    for(i = 1; i < array.length; i++){
+      if (minimal > array[i]) {
+        minimal = array[i];
+      }
+    }
+    return minimal;
+  }
+  
+  exports.max = function max (array) {
+    if (array === undefined || array == 0) {
+      return 0;
+    }
+    let maximal = array[0];
+    let i;
+    for(i = 1; i < array.length; i++){
+      if (maximal < array[i]) {
+        maximal = array[i];
+      }
+    }
+    return maximal;
+  }
+  
+  exports.avg = function avg (array) {
+    if (array === undefined || array == 0) {
+      return 0;
+    }
+    let total = array.reduce(function (a, b) {return a + b});
+    let average = total / array.length;
+    return average;
+  }
